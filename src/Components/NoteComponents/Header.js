9993163c47646c7d87b52471af287779f7b213c0
ChhayaTomar <101592644+ChhayaTomar@users.noreply.github.com>
2022-10-12 14:41:41 +0530

    first commit

---
+++ src/Components/NoteComponents/Header.js
@@ -0,0 +1,11 @@
+import '../css/Header.css'
+function Header() {
+    return (
+      <div className="header">
+        <h1 className="notes__title">Notes <sub className="creator">by Chhaya Tomar</sub></h1>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
